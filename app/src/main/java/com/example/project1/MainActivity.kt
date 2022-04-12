d8793c891601846703eb2e36876fb50b34f3de5c
ely-hdz <elizabeth.hernandez-galvan@scholar.smash.org>
2022-04-11 18:06:22 -0700

    Phase1List

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -4,6 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val events = listOf(
+        Event(
+            foodTruckName: "Authentic Street Taco"
+            location: "Silo Patio"
+            time: "11am - 4pm"
+            foodTruckDesc: "A unique blend of street taco with high-quality\n" +
+                "ingredients to give you the highest quality\n" + "Mexican food."
+            foodTruckWebsite: "https://www.authenticstreettaco.com/"
+        )
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
