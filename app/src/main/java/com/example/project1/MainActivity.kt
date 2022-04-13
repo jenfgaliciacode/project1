1cf14a47070bc15e71f3b29062f376c0b62d958a
Jen Galicia <jfgalicia@ucdavis.edu>
2022-04-12 21:13:24 -0700

    Phase1 Data Binding

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -43,9 +44,11 @@ class MainActivity : AppCompatActivity() {
         ),
     )
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        var view = binding.root
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        var view = binding.root
         setContentView(view)
+
     }
 }
