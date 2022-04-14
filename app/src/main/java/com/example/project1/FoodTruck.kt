a67bdcd070149382d95885d30602d6a57449ec39
ely-hdz <elizabeth.hernandez-galvan@scholar.smash.org>
2022-04-13 22:04:52 -0700

    Phase2

--- app/src/main/java/com/example/project1/FoodTruck.kt
+++ app/src/main/java/com/example/project1/FoodTruck.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class FoodTruck : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_truck)
+        setContentView(R.layout.activity_food_truck_detail)
     }
 }
